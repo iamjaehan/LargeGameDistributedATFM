570464d336e9fec084e0f8ab75f55723768acf55
jaehan im <jaehan.im@utexas.edu>
2025-07-28 21:53:11 -0500

    n Test update

--- Devel/MC_nTest.m
+++ Devel/MC_nTest.m
@@ -6,12 +6,15 @@ for i = 1:caseNum
     n = nCaseSet(i);
     epsilon = 1;
     algorithm = 1;
+    disp("Ours with "+num2str(n)+" players.")
     run("Main.m")
 
     epsilon = 0;
     algorithm = 1;
+    disp("Round Robin with "+num2str(n)+" players.")
     run("Main.m")
 
     algorithm = 2;
+    disp("Centralized with "+num2str(n)+" players.")
     run("Main.m")
 end
